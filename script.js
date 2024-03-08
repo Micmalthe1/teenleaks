bfb553ec516c1ee394e552b670ef12e75ddc50ed
Micmalthe1 <Zielinskim296@gmail.com>
2024-03-08 17:59:19 +0000

    d

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// script.js
 
-// Function to read a random line from a file
 function getRandomLine() {
-    fetch('/get-random-line') // Endpoint for server-side logic (defined in Python)
+    fetch('/get-random-line') 
         .then(response => response.text())
         .then(data => {
             const randomLine = data;
 
-            // Display the random line on the webpage
             document.getElementById('randomLine').textContent = randomLine;
         })
         .catch(error => console.error('Error fetching random line:', error));
--- server.py
+++ server.py
@@ -1,14 +1,12 @@
-# server.py
+
 
 import http.server
 import socketserver
 import random
 
-# Read lines from the document
 with open('keys.txt', 'r') as file:
     lines = file.readlines()
 
-# Define a handler to serve the random line
 class RandomLineHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -18,16 +16,15 @@ class RandomLineHandler(http.server.SimpleHTTPRequestHandler):
         # Choose a random line
         random_line = random.choice(lines)
 
-        # Remove the chosen line from the list (optional)
+
         lines.remove(random_line)
 
-        # Save the updated content back to the file (optional)
+
         with open('keys.txt', 'w') as updated_file:
             updated_file.writelines(lines)
 
         self.wfile.write(random_line.encode())
 
-# Start the server
 port = 8000
 httpd = socketserver.TCPServer(('', port), RandomLineHandler)
 print(f'Serving on port {port}...')
